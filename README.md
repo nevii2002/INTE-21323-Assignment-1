300aa225f684e11232cba09c30efa21f97a0c23f
nevi2002 <munasin-im22042@stu.kln.ac.lk>
2025-03-06 13:43:16 +0530

    Initial commit: Added calculator script and README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Calculator_ Nevindi 
+
+This Python code creates a function to add two numbers and prints the result
---
+++ calculator.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    num1 = 5
+    num2 = 3
+    print(f"Sum: {add(num1, num2)}")
